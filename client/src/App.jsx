25ba6881936cdd4118810032cc611fe20d744906
khushibora <114758263+khushibora@users.noreply.github.com>
2025-11-25 21:07:33 +0530

    routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Routes, Route } from "react-router-dom";
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Routes>
+      
+      </Routes>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
